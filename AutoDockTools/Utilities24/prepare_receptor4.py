758d13ccc429b1bf27bd7cdfb8a01952a19da07e
Leonard Lee <leonardlee@Leonards-MacBook-Air.local>
2023-04-16 21:20:41 +0100

    Implement prepare_receptor as module

--- AutoDockTools/Utilities24/prepare_receptor4.py
+++ AutoDockTools/Utilities24/prepare_receptor4.py
@@ -13,6 +13,8 @@
 #                                                                                                  #
 # ##################################################################################################
 
+__all__ = ["prepare_receptor"]
+
 import os
 
 from MolKit import Read
@@ -21,11 +23,135 @@ import MolKit.protein
 from AutoDockTools.MoleculePreparation import AD4ReceptorPreparation
 
 
+def prepare_receptor(
+    receptor_filename: str,
+    verbose: bool = False,
+    outputfilename: str = None,
+    repairs: str = "",
+    charges_to_add: str = "gasteiger",
+    preserve_charge_types: str = None,
+    cleanup: str = "nphs_lps_waters_nonstdres",
+    delete_single_nonstd_residues: str = None,
+    mode: str = "automatic",
+    dictionary: str = None,
+    unique_atom_names: bool = False,
+):
+    """Prepare a receptor for an AutoDock experiment.
+
+    :param receptor_filename: supported file types include pdb,mol2,pdbq,pdbqs,pdbqt, possibly pqr,cif
+    :param verbose: verbose output, defaults to False
+    :param outputfilename: defaults to "molecule_name.pdbqt"
+    :param repairs: type(s) of repairs to make:
+        'bonds_hydrogens': build bonds and add hydrogens,
+        'bonds': build a single bond from each atom with no bonds to its closest neighbor
+        'hydrogens': add hydrogens
+        'checkhydrogens': add hydrogens only if there are none already
+        'None': do not make any repairs
+        defaults to "None"
+    :param charges_to_add: preserve all input charges ie do not add new charges,
+        defaults to "gasteiger", addition of gasteiger charges
+    :param preserve_charge_types: preserve input charges on specific atom types, eg -p Zn -p Fe, defaults to None
+    :param cleanup: cleanup type:
+        'nphs': merge charges and remove non-polar hydrogens
+        'lps': merge charges and remove lone pairs
+        'waters': remove water residues
+        'nonstdres': remove chains composed entirely of residues of types other than the standard 20 amino acids
+        'deleteAltB': remove XX@B atoms and rename XX@A atoms->XX
+        defaults to "nphs_lps_waters_nonstdres"
+    :param delete_single_nonstd_residues: delete every nonstd residue from any chain,
+        'True': any residue whose name is not in this list:
+            ['CYS','ILE','SER','VAL','GLN','LYS','ASN',
+            'PRO','THR','PHE','ALA','HIS','GLY','ASP',
+            'LEU', 'ARG', 'TRP', 'GLU', 'TYR','MET',
+            'HID', 'HSP', 'HIE', 'HIP', 'CYX', 'CSS']
+        will be deleted from any chain.
+        NB: there are no  nucleic acid residue names at all in the list and no metals. 
+        defaults to False
+    :param mode: interactive, defaults to "automatic": outputfile is written with no further user input)
+    :param dictionary:  file to contain receptor summary information, defaults to None
+    :param unique_atom_names: assign each receptor atom a unique name: newname is original name plus its index(1-based), defaults to False
+    """
+    mols = Read(receptor_filename)
+    if verbose:
+        print('read ', receptor_filename)
+    mol = mols[0]
+    if unique_atom_names:  # added to simplify setting up covalent dockings 8/2014
+        for at in mol.allAtoms:
+            if mol.allAtoms.get(at.name) > 1:
+                at.name = at.name + str(at._uniqIndex + 1)
+        if verbose:
+            print("renamed %d atoms: each newname is the original name of the atom plus its (1-based) uniqIndex" %
+                  (len(mol.allAtoms)))
+    preserved = {}
+    has_autodock_element = False
+    if charges_to_add and preserve_charge_types:
+        if hasattr(mol, 'allAtoms') and not hasattr(mol.allAtoms[0], 'autodock_element'):
+            file_name, file_ext = os.path.splitext(receptor_filename)
+            if file_ext == '.pdbqt':
+                has_autodock_element = True
+        if preserve_charge_types and not has_autodock_element:
+            print('prepare_receptor4: input format does not have autodock_element SO unable to preserve charges on ' + preserve_charge_types)
+            print('exiting...')
+            sys.exit(1)
+        preserved_types = preserve_charge_types.split(',')
+        if verbose:
+            print("preserved_types=", preserved_types)
+        for t in preserved_types:
+            if verbose:
+                print('preserving charges on type->', t)
+            if not len(t):
+                continue
+            ats = mol.allAtoms.get(lambda x: x.autodock_element == t)
+            if verbose:
+                print("preserving charges on ", ats.name)
+            for a in ats:
+                if a.chargeSet is not None:
+                    preserved[a] = [a.chargeSet, a.charge]
+
+    if len(mols) > 1:
+        if verbose:
+            print("more than one molecule in file")
+        # use the molecule with the most atoms
+        ctr = 1
+        for m in mols[1:]:
+            ctr += 1
+            if len(m.allAtoms) > len(mol.allAtoms):
+                mol = m
+                if verbose:
+                    print("mol set to ", ctr, "th molecule with",
+                          len(mol.allAtoms), "atoms")
+    mol.buildBondsByDistance()
+    alt_loc_ats = mol.allAtoms.get(lambda x: "@" in x.name)
+    len_alt_loc_ats = len(alt_loc_ats)
+    if len_alt_loc_ats:
+        print("WARNING!", mol.name, "has", len_alt_loc_ats,
+              ' alternate location atoms!\nUse prepare_pdb_split_alt_confs.py to create pdb files containing a single conformation.\n')
+
+    if verbose:
+        print("setting up RPO with mode=", mode, end=' ')
+        print("and outputfilename= ", outputfilename)
+        print("charges_to_add=", charges_to_add)
+        print("delete_single_nonstd_residues=", delete_single_nonstd_residues)
+
+    RPO = AD4ReceptorPreparation(mol, mode, repairs, charges_to_add,
+                                 cleanup, outputfilename=outputfilename,
+                                 preserved=preserved,
+                                 delete_single_nonstd_residues=delete_single_nonstd_residues,
+                                 dict=dictionary)
+
+    if charges_to_add:
+        # restore any previous charges
+        for atom, chargeList in list(preserved.items()):
+            atom._charges[chargeList[0]] = chargeList[1]
+            atom.chargeSet = chargeList[0]
+
+
 if __name__ == '__main__':
     import sys
     import getopt
 
 
+    # autopep8: off
     def usage():
         "Print helpful, accurate usage statement to stdout."
         print("Usage: prepare_receptor4.py -r filename")
@@ -154,73 +280,19 @@ if __name__ == '__main__':
 
     #what about nucleic acids???
 
-    mols = Read(receptor_filename)
-    if verbose: print('read ', receptor_filename)
-    mol = mols[0]
-    if unique_atom_names:  # added to simplify setting up covalent dockings 8/2014
-        for at in mol.allAtoms:
-            if mol.allAtoms.get(at.name) >1:
-                at.name = at.name + str(at._uniqIndex +1)
-        if verbose:
-            print("renamed %d atoms: each newname is the original name of the atom plus its (1-based) uniqIndex" %(len(mol.allAtoms)))        
-    preserved = {}
-    has_autodock_element = False
-    if charges_to_add and preserve_charge_types:
-        if hasattr(mol, 'allAtoms') and not hasattr(mol.allAtoms[0], 'autodock_element'):
-            file_name, file_ext = os.path.splitext(receptor_filename)
-            if file_ext == '.pdbqt':
-                has_autodock_element = True
-        if preserve_charge_types and not has_autodock_element:
-            print('prepare_receptor4: input format does not have autodock_element SO unable to preserve charges on ' + preserve_charge_types)
-            print('exiting...')
-            sys.exit(1)
-        preserved_types = preserve_charge_types.split(',') 
-        if verbose: print("preserved_types=", preserved_types)
-        for t in preserved_types:
-            if verbose:
-                print('preserving charges on type->', t)
-            if not len(t):
-                continue
-            ats = mol.allAtoms.get(lambda x: x.autodock_element==t)
-            if verbose:
-                print("preserving charges on ", ats.name)
-            for a in ats:
-                if a.chargeSet is not None:
-                    preserved[a] = [a.chargeSet, a.charge]
-
-    if len(mols)>1:
-        if verbose: print("more than one molecule in file")
-        #use the molecule with the most atoms
-        ctr = 1
-        for m in mols[1:]:
-            ctr += 1
-            if len(m.allAtoms)>len(mol.allAtoms):
-                mol = m
-                if verbose: print("mol set to ", ctr, "th molecule with", len(mol.allAtoms), "atoms")
-    mol.buildBondsByDistance()
-    alt_loc_ats = mol.allAtoms.get(lambda x: "@" in x.name)
-    len_alt_loc_ats = len(alt_loc_ats)
-    if len_alt_loc_ats:
-        print("WARNING!", mol.name, "has",len_alt_loc_ats, ' alternate location atoms!\nUse prepare_pdb_split_alt_confs.py to create pdb files containing a single conformation.\n')
-
-    if verbose:
-        print("setting up RPO with mode=", mode, end=' ')
-        print("and outputfilename= ", outputfilename)
-        print("charges_to_add=", charges_to_add)
-        print("delete_single_nonstd_residues=", delete_single_nonstd_residues)
-
-    RPO = AD4ReceptorPreparation(mol, mode, repairs, charges_to_add, 
-                        cleanup, outputfilename=outputfilename,
-                        preserved=preserved, 
-                        delete_single_nonstd_residues=delete_single_nonstd_residues,
-                        dict=dictionary)    
-
-    if charges_to_add:
-        #restore any previous charges
-        for atom, chargeList in list(preserved.items()):
-            atom._charges[chargeList[0]] = chargeList[1]
-            atom.chargeSet = chargeList[0]
-
+    prepare_receptor(
+        receptor_filename=receptor_filename,
+        verbose=verbose,
+        outputfilename=outputfilename,
+        repairs=repairs,
+        charges_to_add=charges_to_add,
+        preserve_charge_types=preserve_charge_types,
+        cleanup=cleanup,
+        delete_single_nonstd_residues=delete_single_nonstd_residues,
+        mode=mode,
+        dictionary=dictionary,
+        unique_atom_names=unique_atom_names
+    )
 
 # To execute this command type:
 # prepare_receptor4.py -r pdb_file -o outputfilename -A checkhydrogens 
